fad44553df6b4c2a524349e9ef28c05a16cb6e6c
Josyan Pierson <josyan.pierson@gmail.com>
2011-10-15 16:00:46 -0400

    Add some interractivity on door_frames

--- public/javascripts/door_lines.js
+++ public/javascripts/door_lines.js
@@ -2,4 +2,12 @@
 var $j = jQuery;
 $j(document).ready(function(){
 
+  // door frame interraction
+  $j('#door-frame-selection .door-frame').click(function(){
+    id = $j(this).attr('id').replace('df-', '');
+
+    $j('#door_line_door_frame_id').val(id);
+    $j('#door-combination-selection .door-combination-list').hide();
+    $j('#door-combination-selection #dcl-' + id).show();
+  });
 });
