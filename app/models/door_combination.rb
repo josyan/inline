c23aeec491db04d94257ebd74cfe451dc55e902b
Josyan Pierson <josyan.pierson@gmail.com>
2012-01-08 16:32:43 -0500

    Link DoorCombination and DoorOpening models

--- app/models/door_combination.rb
+++ app/models/door_combination.rb
@@ -4,4 +4,5 @@ class DoorCombination < ActiveRecord::Base
   validates_presence_of :name, :sections, :preview_image_name, :door_frame_id
 
   belongs_to :door_frame
+  has_and_belongs_to_many :door_openings
 end
--- app/models/door_opening.rb
+++ app/models/door_opening.rb
@@ -1,2 +1,3 @@
 class DoorOpening < ActiveRecord::Base
+  has_and_belongs_to_many :door_combinations
 end
