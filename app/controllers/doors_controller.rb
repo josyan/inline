bd21ce3b7d8b8e28cf2c5df98ef195f9bd60f540
Josyan Pierson <josyan.pierson@gmail.com>
2012-01-22 15:50:43 -0500

    Display section dimension select box

--- app/controllers/doors_controller.rb
+++ app/controllers/doors_controller.rb
@@ -29,12 +29,15 @@ class DoorsController < ApplicationController
     @door_line_sections = door_combination.sections.split(';').map do |door_section_code|
       door_line_section = { :door_section => DoorSection.find_by_code(door_section_code) }
       door_line_section[:door_panels] = door_line_section[:door_section].door_panels
+      door_line_section[:door_section_dimensions] = door_line_section[:door_section].door_section_dimensions
       door_line_section[:door_line_section] = DoorLineSection.new(:door_section => door_line_section[:door_section],
-                                                                  :door_panel => door_line_section[:door_panels].first)
+                                                                  :door_panel => door_line_section[:door_panels].first,
+                                                                  :door_section_dimension => door_line_section[:door_section_dimensions].first)
       # check if we can somewhat recopy the previous configuration in the new one
       possible_choices = previous_sections.select { |s| s[:door_section_id] == door_line_section[:door_section].id.to_s }
       if possible_choices.length >= 1
         door_line_section[:door_line_section].door_panel_id = possible_choices[0][:door_panel_id].to_i unless possible_choices[0][:door_panel_id].blank?
+        door_line_section[:door_line_section].door_section_dimension_id = possible_choices[0][:door_section_dimension_id].to_i unless possible_choices[0][:door_section_dimension_id].blank?
         if possible_choices.length > 1
           previous_sections.each_with_index do |section, index|
             if section[:door_section_id] == possible_choices.first[:door_section_id]
