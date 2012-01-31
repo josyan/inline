4f27a435e95b9cea55209dd1450d2510bc8aab7f
Josyan Pierson <josyan.pierson@gmail.com>
2012-01-30 20:39:17 -0500

    Some adjustments for options menu

--- app/models/option.rb
+++ app/models/option.rb
@@ -6,6 +6,7 @@ class Option < ActiveRecord::Base
   has_and_belongs_to_many :series, :class_name => 'Serie' # :class_name should not be here, but 'series'.singularize = 'series' for rails, which is not correct
   has_many :options_quotation_lines
   has_many :options_quotations
+  belongs_to :module_type
 
   has_and_belongs_to_many :option_categories, :order => "option_categories.display_order asc"
 
