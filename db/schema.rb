1da2c06814df87e79e98a713dde509993ac1aa15
Josyan Pierson <josyan.pierson@gmail.com>
2012-02-27 20:18:34 -0500

    Add price to door combinations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120228003825) do
+ActiveRecord::Schema.define(:version => 20120228011634) do
 
   create_table "companies", :force => true do |t|
     t.string  "name",              :limit => 100, :default => "", :null => false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(:version => 20120228003825) do
     t.integer  "door_frame_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "price",              :default => 0.0
   end
 
   create_table "door_combinations_door_openings", :id => false, :force => true do |t|
