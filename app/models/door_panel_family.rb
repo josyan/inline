a5bbc1ce122c762ca8343e6b447d9ec3f7a97e99
Josyan Pierson <josyan.pierson@gmail.com>
2012-03-19 22:06:39 -0400

    Modify table sctructure and repopulate data for panel families and panels

--- app/models/door_panel_family.rb
+++ app/models/door_panel_family.rb
@@ -1,4 +1,4 @@
 class DoorPanelFamily < ActiveRecord::Base
   belongs_to :slab_material
-  has_many :door_panels
+  has_many :door_panels, :dependent => :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320003157) do
+ActiveRecord::Schema.define(:version => 20120320015100) do
 
   create_table "companies", :force => true do |t|
     t.string  "name",              :limit => 100, :default => "", :null => false
@@ -173,7 +173,8 @@ ActiveRecord::Schema.define(:version => 20120320003157) do
     t.string   "preview_image_name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "price",              :default => 0.0
+    t.float    "price",                :default => 0.0
+    t.integer  "door_panel_family_id"
   end
 
   create_table "door_panels_door_sections", :id => false, :force => true do |t|
