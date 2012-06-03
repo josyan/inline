9a14fe1d668ce9c0ace86e853e85def20d49b147
Josyan Pierson <josyan.pierson@gmail.com>
2012-06-03 15:30:33 -0400

    Remove data related to door_section_dimensions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120526004135) do
+ActiveRecord::Schema.define(:version => 20120603192544) do
 
   create_table "companies", :force => true do |t|
     t.string  "name",              :limit => 100, :default => "", :null => false
@@ -132,7 +132,6 @@ ActiveRecord::Schema.define(:version => 20120526004135) do
     t.integer  "door_glass_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "door_section_dimension_id"
   end
 
   create_table "door_lines", :force => true do |t|
@@ -180,14 +179,6 @@ ActiveRecord::Schema.define(:version => 20120526004135) do
   create_table "door_panels_door_sections", :id => false, :force => true do |t|
     t.integer "door_panel_id"
     t.integer "door_section_id"
-  end
-
-  create_table "door_section_dimensions", :force => true do |t|
-    t.integer  "door_section_id"
-    t.float    "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.float    "price",           :default => 0.0
   end
 
   create_table "door_sections", :force => true do |t|
