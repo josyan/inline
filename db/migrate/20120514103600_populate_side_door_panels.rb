bf0383e1cf392bcfaa94084a98aba51172a4be9e
Josyan Pierson <josyan.pierson@gmail.com>
2012-06-03 18:08:47 -0400

    Ensure all sections have at least one panel

--- db/migrate/20120514103600_populate_side_door_panels.rb
+++ db/migrate/20120514103600_populate_side_door_panels.rb
@@ -1,7 +1,7 @@
 class PopulateSideDoorPanels < ActiveRecord::Migration
   def self.up
     DoorPanelFamily.all.each do |door_panel_family|
-      [50, 51, 53, 55, 56, 60].each do |i|
+      [50, 51, 53, 55, 56, 60, 61].each do |i|
         DoorPanel.create :name => "Panneau #{i}", :preview_image_name => "panel_#{'%02d' % i}.png", :door_panel_family_id => door_panel_family.id
       end
     end
