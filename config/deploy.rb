b90cede060055e7c2f0dfa9e0f3b5b794e69e1a0
Josyan Pierson <josyan.pierson@gmail.com>
2012-07-25 21:57:06 -0400

    Capify application

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "inline"
+set :repository,  "git://github.com/josyan/inline.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "quotations.glass-vision.net"                          # Your HTTP server, Apache/etc
+role :app, "quotations.glass-vision.net"                          # This may be the same as your `Web` server
+role :db,  "quotations.glass-vision.net", :primary => true
+
+set :deploy_to, '/home/glass/apps/inline'
+set :user, 'glass'
+set :use_sudo, false
+
+namespace(:inline) do
+  task :symlink, :roles => :web do
+    run <<-CMD
+      ln -nfs #{shared_path}/public/system #{release_path}/public/system
+    CMD
+  end
+end
+after "deploy:symlink", "inline:symlink"
