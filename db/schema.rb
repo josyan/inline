009d688637fec87712deeec0d93b205de53b87f5
Josyan Pierson <josyan.pierson@gmail.com>
2012-09-11 21:16:00 -0400

    Populate default gap value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912011040) do
+ActiveRecord::Schema.define(:version => 20120912011330) do
 
   create_table "companies", :force => true do |t|
     t.string  "name",              :limit => 100, :default => "", :null => false
